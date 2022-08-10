afbef7c5822fb76deee7889182e023a44d188137
Mirlan Jelamanov <mirlan.jelamanov@gmail.com>
2022-08-10 17:27:46 +0600

    Adds refund feature

--- order/backend/order.factoring004.admin.order.php
+++ order/backend/order.factoring004.admin.order.php
@@ -3,8 +3,11 @@
 use BnplPartners\Factoring004\Exception\ErrorResponseException;
 use BnplPartners\Factoring004\Exception\PackageException;
 use BnplPartners\Factoring004Diafan\Handler\DeliveryHandler;
+use BnplPartners\Factoring004Diafan\Handler\FullRefundHandler;
+use BnplPartners\Factoring004Diafan\Handler\PartialRefundHandler;
 use BnplPartners\Factoring004Diafan\Helper\SessionTrait;
 use BnplPartners\Factoring004Diafan\Otp\DeliveryOtpChecker;
+use BnplPartners\Factoring004Diafan\Otp\RefundOtpChecker;
 
 if (!defined('DIAFAN')) {
     $path = __FILE__;
@@ -69,6 +72,25 @@ class Order_factoring004_admin_order extends Diafan
                 return;
             }
 
+            $amount = $this->calculateAmount($_POST);
+
+            if ($amount > $order['summ']) {
+                $this->putSession('error_' . $this->diafan->id, 'Сумма товаров изменилась в большую сторону');
+                $this->diafan->redirect($this->getBackUrl());
+                exit;
+            }
+
+            if ($amount < $order['summ']) {
+                $redirectLink = $this->partialRefund($order, $amount);
+
+                if ($redirectLink) {
+                    $this->diafan->redirect($redirectLink);
+                    exit;
+                }
+
+                return;
+            }
+
             $redirectLink = $this->changeOrderStatus($order, $_POST['status_id']);
 
             if ($redirectLink) {
@@ -124,6 +146,28 @@ class Order_factoring004_admin_order extends Diafan
 
     /**
      * @param array<string, mixed> $order
+     * @param float $amount
+     *
+     * @return string|null
+     *
+     * @throws \BnplPartners\Factoring004\Exception\PackageException
+     */
+    private function partialRefund(array $order, $amount)
+    {
+        $handler = new PartialRefundHandler();
+
+        $shouldConfirmOtp = $handler->handle($order, (int) ceil($amount));
+
+        if ($shouldConfirmOtp) {
+            $this->putSession('send_otp_' . $this->diafan->id, true);
+            return $this->getBackUrl();
+        }
+
+        return null;
+    }
+
+    /**
+     * @param array<string, mixed> $order
      * @param string $statusId
      *
      * @return string|null
@@ -149,6 +193,68 @@ class Order_factoring004_admin_order extends Diafan
     }
 
     /**
+     * @param array<string, mixed> $request
+     *
+     * @return float
+     */
+    private function calculateAmount(array $request)
+    {
+        $products = [];
+        $additional = [];
+
+        foreach ($request as $key => $value) {
+            if (strpos($key, 'price_goods') !== false) {
+                $id = substr($key, strlen('price_goods'));
+
+                if (isset($products[$id])) {
+                    $products[$id]['price'] = $value;
+                } else {
+                    $products[$id] = ['price' => $value];
+                }
+            }
+
+            if (strpos($key, 'count_goods') !== false) {
+                $id = substr($key, strlen('count_goods'));
+
+                if (isset($products[$id])) {
+                    $products[$id]['count'] = $value;
+                } else {
+                    $products[$id] = ['count' => $value];
+                }
+            }
+
+            if (strpos($key, 'additional_cost_id') !== false) {
+                $id = substr($key, strlen('additional_cost_id'));
+
+                if (isset($additional[$id])) {
+                    $additional[$id]['count'] = $value;
+                } else {
+                    $additional[$id] = ['count' => $value];
+                }
+            }
+
+            if (strpos($key, 'summ_additional_cost') !== false) {
+                $id = substr($key, strlen('summ_additional_cost'));
+
+                if (isset($additional[$id])) {
+                    $additional[$id]['price'] = $value;
+                } else {
+                    $additional[$id] = ['price' => $value];
+                }
+            }
+        }
+
+        $items = array_merge(array_values($products), array_values($additional));
+        $amount = array_reduce($items, function ($prev, $curr) {
+            $count = isset($curr['count']) ? $curr['count'] : 1;
+
+            return $prev + $count * $curr['price'];
+        }, 0);
+
+        return ($amount + $request['delivery_summ']) - $request['custom_discount_summ'];
+    }
+
+    /**
      * @param string $currentOrderStatus
      * @param string $newOrderStatus
      *
@@ -160,6 +266,10 @@ class Order_factoring004_admin_order extends Diafan
     {
         if ($newOrderStatus === '4') {
             return new DeliveryHandler();
+        }
+
+        if ($newOrderStatus === '3' && $currentOrderStatus === '4') {
+            return new FullRefundHandler();
         }
 
         throw new InvalidArgumentException('Order status handler not found');
@@ -176,7 +286,7 @@ class Order_factoring004_admin_order extends Diafan
             return new DeliveryOtpChecker();
         }
 
-        throw new InvalidArgumentException('OTP checker not found');
+        return new RefundOtpChecker();
     }
 
     /**
